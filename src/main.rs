6487e1b4a3e6346e8dc87e2ab67320632c9750b1
Sebastian Ramacher <sebastian@ramacher.at>
2021-11-14 23:47:53 +0100

    Add remaining architectures

--- src/main.rs
+++ src/main.rs
@@ -41,15 +41,31 @@ enum Verdict {
 #[serde(rename_all = "lowercase")]
 enum Architecture {
     All,
+    Alpha,
     Amd64,
     Arm64,
     Armel,
     Armhf,
+    Hppa,
+    #[serde(rename = "hurd-i386")]
+    HurdI386,
     I386,
+    #[serde(rename = "kfreebsd-amd64")]
+    KFreeBSDAmd64,
+    #[serde(rename = "kfreebsd-i386")]
+    KFreeBSDI386,
+    M86k,
     Mips64el,
     Mipsel,
+    PowerPC,
+    Ppc64,
     Ppc64el,
+    Riscv64,
     S390x,
+    Sh4,
+    Sparc64,
+    X32,
+    ia64,
 }
 
 impl Display for Architecture {
@@ -59,15 +75,28 @@ impl Display for Architecture {
             "{}",
             match self {
                 Architecture::All => "all",
+                Architecture::Alpha => "alpha",
                 Architecture::Amd64 => "amd64",
                 Architecture::Arm64 => "arm64",
                 Architecture::Armel => "armel",
                 Architecture::Armhf => "armhf",
+                Architecture::Hppa => "hppa",
+                Architecture::HurdI386 => "hurd-i386",
                 Architecture::I386 => "i386",
+                Architecture::KFreeBSDAmd64 => "kfreebsd-amd64",
+                Architecture::KFreeBSDI386 => "kfreebsd-i386",
+                Architecture::M86k => "m86k",
                 Architecture::Mips64el => "mips64el",
                 Architecture::Mipsel => "mipsel",
+                Architecture::PowerPC => "powerpc",
+                Architecture::Ppc64 => "ppc64",
                 Architecture::Ppc64el => "ppc64el",
+                Architecture::Riscv64 => "risc64",
                 Architecture::S390x => "s390x",
+                Architecture::Sh4 => "sh4",
+                Architecture::Sparc64 => "sparc64",
+                Architecture::X32 => "x32",
+                Architecture::ia64 => "ia64",
             }
         )
     }
