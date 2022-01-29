192fd50aefa4fff213662491e19db0891abcf99c
Sebastian Ramacher <sebastian@ramacher.at>
2022-01-29 18:44:02 +0100

    binnmu-buildinfo: skip Arch: all

--- src/binnmu_buildinfo.rs
+++ src/binnmu_buildinfo.rs
@@ -10,9 +10,9 @@ use clap::Parser;
 
 use crate::{config::Cache, source_packages::SourcePackages, BaseOptions};
 use assorted_debian_utils::{
-    architectures::RELEASE_ARCHITECTURES,
-    buildinfo,
-    wb::{BinNMU, SourceSpecifier, WBCommandBuilder},
+    architectures::{Architecture, RELEASE_ARCHITECTURES},
+    buildinfo::{self, Buildinfo},
+    wb::{BinNMU, SourceSpecifier, WBCommand, WBCommandBuilder},
 };
 
 #[derive(Debug, Parser)]
@@ -50,6 +50,43 @@ impl BinNMUBuildinfo {
         }
     }
 
+    fn process(&self, buildinfo: Buildinfo, source_packages: &SourcePackages) -> WBCommand {
+        let mut source_split = buildinfo.source.split_whitespace();
+        let source_package = source_split.next().unwrap();
+
+        let mut version_split = buildinfo.version.split("+b");
+        let version = version_split.next().unwrap();
+
+        // let mut nmu_version = None;
+        let mut source = SourceSpecifier::new(source_package);
+        source.with_version(version).with_suite(&self.options.suite);
+        if !source_packages.is_ma_same(source_package) {
+            // binNMU only on the architecture if no MA: same binary packages
+            source.with_archive_architectures(&[buildinfo.architecture]);
+            //  } else {
+            //      if let Some(binnmu_version) = version_split.next() {
+            //          nmu_version = Some(binnmu_version.parse::<u32>().unwrap() + 1);
+            //      } else {
+            //          nmu_version = Some(1u32);
+            //      }
+        }
+
+        let mut binnmu = BinNMU::new(&source, &self.options.message);
+        if let Some(bp) = self.options.build_priority {
+            binnmu.with_build_priority(bp);
+        }
+        if let Some(dw) = &self.options.dep_wait {
+            binnmu.with_dependency_wait(dw);
+        }
+        if let Some(extra_depends) = &self.options.extra_depends {
+            binnmu.with_extra_depends(extra_depends);
+        }
+        //  if let Some(version) = nmu_version {
+        //      binnmu.with_nmu_version(version);
+        //  }
+        binnmu.build()
+    }
+
     pub(crate) fn run(self) -> Result<()> {
         let cache = Cache::new()?;
         let mut all_paths = vec![];
@@ -60,44 +97,23 @@ impl BinNMUBuildinfo {
 
         let mut wb_commands = HashSet::new();
         // iterate over all buildinfo files
-        for filename in self.options.inputs {
-            let data = strip_signature(BufReader::new(File::open(filename)?))?;
-            let buildinfo = buildinfo::from_reader(data.as_ref())?;
-
-            let mut source_split = buildinfo.source.split_whitespace();
-            let source_package = source_split.next().unwrap();
-
-            let mut version_split = buildinfo.version.split("+b");
-            let version = version_split.next().unwrap();
-
-            // let mut nmu_version = None;
-            let mut source = SourceSpecifier::new(source_package);
-            source.with_version(version).with_suite(&self.options.suite);
-            if !source_packages.is_ma_same(source_package) {
-                // binNMU only on the architecture if no MA: same binary packages
-                source.with_archive_architectures(&[buildinfo.architecture]);
-                //  } else {
-                //      if let Some(binnmu_version) = version_split.next() {
-                //          nmu_version = Some(binnmu_version.parse::<u32>().unwrap() + 1);
-                //      } else {
-                //          nmu_version = Some(1u32);
-                //      }
+        for filename in &self.options.inputs {
+            let data = strip_signature(BufReader::new(File::open(&filename)?))?;
+            match buildinfo::from_reader(data.as_ref()) {
+                Err(e) => {
+                    println!("# skipping {}: {}", filename.display(), e);
+                    continue;
+                }
+                Ok(bi) => {
+                    if bi.architecture == Architecture::All {
+                        println!(
+                            "# skipping {}: architecture all not binnumable",
+                            filename.display()
+                        )
+                    }
+                    wb_commands.insert(self.process(bi, &source_packages));
+                }
             }
-
-            let mut binnmu = BinNMU::new(&source, &self.options.message);
-            if let Some(bp) = self.options.build_priority {
-                binnmu.with_build_priority(bp);
-            }
-            if let Some(dw) = &self.options.dep_wait {
-                binnmu.with_dependency_wait(dw);
-            }
-            if let Some(extra_depends) = &self.options.extra_depends {
-                binnmu.with_extra_depends(extra_depends);
-            }
-            //  if let Some(version) = nmu_version {
-            //      binnmu.with_nmu_version(version);
-            //  }
-            wb_commands.insert(binnmu.build());
         }
 
         for commands in wb_commands {
