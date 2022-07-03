abadc875f569e950b9ca68cbdf8946c6c72119d1
Sebastian Ramacher <sramacher@debian.org>
2022-07-03 09:24:19 +0200

    assorted-debian-utils: make datetime format string 'static

--- assorted-debian-utils/src/utils.rs
+++ assorted-debian-utils/src/utils.rs
@@ -8,23 +8,22 @@ use std::fmt;
 use chrono::{DateTime, TimeZone, Utc};
 use serde::de;
 
+/// Helper to parse date-time strings as UTC based on a given format
 #[derive(Debug)]
-pub(crate) struct DateTimeVisitor<'a>(pub &'a str);
+pub(crate) struct DateTimeVisitor(pub &'static str);
 
-impl<'de> de::Visitor<'de> for DateTimeVisitor<'_> {
+impl<'de> de::Visitor<'de> for DateTimeVisitor {
     type Value = DateTime<Utc>;
 
     fn expecting(&self, formatter: &mut fmt::Formatter) -> fmt::Result {
-        write!(formatter, "a date and time formatted as {}", self.0)
+        write!(formatter, "a date and time formatted as '{}'", self.0)
     }
 
-    fn visit_str<E>(self, s: &str) -> std::result::Result<Self::Value, E>
+    fn visit_str<E>(self, s: &str) -> Result<Self::Value, E>
     where
         E: de::Error,
     {
-        match Utc.datetime_from_str(s, self.0) {
-            Ok(dt) => Ok(dt),
-            Err(_) => Err(de::Error::invalid_value(de::Unexpected::Str(s), &self)),
-        }
+        Utc.datetime_from_str(s, self.0)
+            .map_err(|_| de::Error::invalid_value(de::Unexpected::Str(s), &self))
     }
 }
