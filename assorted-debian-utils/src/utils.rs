99fedadecbef90d415a31c4dca34a31022469498
Sebastian Ramacher <sramacher@debian.org>
2023-09-06 00:13:37 +0200

    assorted-debian-utils: fix deprecation warning

--- assorted-debian-utils/src/utils.rs
+++ assorted-debian-utils/src/utils.rs
@@ -5,7 +5,7 @@
 
 use std::fmt::Formatter;
 
-use chrono::{DateTime, TimeZone, Utc};
+use chrono::{DateTime, NaiveDateTime, TimeZone, Utc};
 use serde::de;
 
 /// Helper to parse date-time strings as UTC based on a given format
@@ -23,7 +23,8 @@ impl<'de> de::Visitor<'de> for DateTimeVisitor {
     where
         E: de::Error,
     {
-        Utc.datetime_from_str(s, self.0)
+        NaiveDateTime::parse_from_str(s, self.0)
             .map_err(|_| de::Error::invalid_value(de::Unexpected::Str(s), &self))
+            .map(|naive_date_time| Utc.from_utc_datetime(&naive_date_time))
     }
 }
